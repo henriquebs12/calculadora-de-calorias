0c0e70ee8618696a54431b38c79e536fdc6a1030
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-07-01 14:11:28 -0300

    arquivo filtros.php, contendo 5 filtros
    5 filtros para a exibicao de alimentos

---
+++ filtros.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once 'assets/php/database.php';
+
+	function getAlimentosOrdemDecrescenteCalorias(){
+		try{
+			$database = new Database();
+			$conn = $database->dbset();
+			$sql = "SELECT `idAlimento`, `nome`,`valor_cal`, `quantidade_proteina`, `quantidade_carboidrato`, `porcao`, `teor_limpidico`, `teor_fibroso`, `Categoria` FROM `Alimento` as A JOIN `TipoAlimento` as B on A.TipoAlimento_idTipo = B.idCategoria ORDER BY valor_cal DESC"; 
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+			$c = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+			return var_dump(json_encode($c)); //Retorna um JSON dos alimentos
+		}catch(Exception $e){
+			echo $e->getMessage();
+			return 0;
+		}
+	}
+
+	function getAlimentosOrdemCrescenteCalorias(){
+		try{
+			$database = new Database();
+			$conn = $database->dbset();
+			$sql = "SELECT `idAlimento`, `nome`,`valor_cal`, `quantidade_proteina`, `quantidade_carboidrato`, `porcao`, `teor_limpidico`, `teor_fibroso`, `Categoria` FROM `Alimento` as A JOIN `TipoAlimento` as B on A.TipoAlimento_idTipo = B.idCategoria ORDER BY valor_cal ASC"; 
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+			$c = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+			return var_dump(json_encode($c)); //Retorna um JSON dos alimentos
+		}catch(Exception $e){
+			echo $e->getMessage();
+			return 0;
+		}
+	}
+
+	function getAlimentosOrdemAlfabetica(){
+		try{
+			$database = new Database();
+			$conn = $database->dbset();
+			$sql = "SELECT `idAlimento`, `nome`,`valor_cal`, `quantidade_proteina`, `quantidade_carboidrato`, `porcao`, `teor_limpidico`, `teor_fibroso`, `Categoria` FROM `Alimento` as A JOIN `TipoAlimento` as B on A.TipoAlimento_idTipo = B.idCategoria ORDER BY nome ASC"; 
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+			$c = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+			return var_dump(json_encode($c)); //Retorna um JSON dos alimentos
+		}catch(Exception $e){
+			echo $e->getMessage();
+			return 0;
+		}
+	}
+
+	function getAlimentosOrdemCrescenteProteinas(){
+		try{
+			$database = new Database();
+			$conn = $database->dbset();
+			$sql = "SELECT `idAlimento`, `nome`,`valor_cal`, `quantidade_proteina`, `quantidade_carboidrato`, `porcao`, `teor_limpidico`, `teor_fibroso`, `Categoria` FROM `Alimento` as A JOIN `TipoAlimento` as B on A.TipoAlimento_idTipo = B.idCategoria ORDER BY quantidade_proteina ASC"; 
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+			$c = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+			return var_dump(json_encode($c)); //Retorna um JSON dos alimentos
+		}catch(Exception $e){
+			echo $e->getMessage();
+			return 0;
+		}
+	}
+
+	function getAlimentosOrdemCrescenteLipideos(){
+		try{
+			$database = new Database();
+			$conn = $database->dbset();
+			$sql = "SELECT `idAlimento`, `nome`,`valor_cal`, `quantidade_proteina`, `quantidade_carboidrato`, `porcao`, `teor_limpidico`, `teor_fibroso`, `Categoria` FROM `Alimento` as A JOIN `TipoAlimento` as B on A.TipoAlimento_idTipo = B.idCategoria ORDER BY teor_lipidico ASC"; 
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+			$c = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+			return var_dump(json_encode($c)); //Retorna um JSON dos alimentos
+		}catch(Exception $e){
+			echo $e->getMessage();
+			return 0;
+		}
+	}
+
+	/*chamando as funcoes*/
+
+	getAlimentosOrdemDecrescenteCalorias();
+	getAlimentosOrdemCrescenteCalorias();
+	getAlimentosOrdemAlfabetica();
+	getAlimentosOrdemCrescenteProteinas();
+	getAlimentosOrdemCrescenteLipideos();
+
+?>
--- getAlimentos.php
+++ getAlimentos.php
@@ -5,15 +5,14 @@ require_once 'assets/php/database.php';
 			$database = new Database();
 			$conn = $database->dbset();
 
-			$sql = "SELECT `idAlimento`, `nome`,`valor_cal`, `quantidade_proteina`, `quantidade_carboidrato`, `porcao`, `teor_limpidico`, `teor_fibroso`, `Categoria` FROM `Alimento` as A JOIN `TipoAlimento` as B on A.TipoAlimento_idTipo = B.idCategoria";
+			$sql = "SELECT `idAlimento`, `nome`,`valor_cal`, `quantidade_proteina`, `quantidade_carboidrato`, `porcao`, `teor_limpidico`, `teor_fibroso`, `Categoria` FROM `Alimento` as A JOIN `TipoAlimento` as B on A.TipoAlimento_idTipo = B.idCategoria"; //Pega todos os alimentos
 			$stmt = $conn->prepare($sql);
 			$stmt->execute();
 			$c = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-			return var_dump(json_encode($c));
+			return var_dump(json_encode($c)); //Retorna um JSON dos alimentos
 		}catch(Exception $e){
 			echo $e->getMessage();
 			return 0;
 		}
 	}
-
-	getAlimentos();
+	getAlimentos(); //Chama o método}
