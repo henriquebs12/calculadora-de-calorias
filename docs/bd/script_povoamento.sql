f338e65eb6b9b239ed6611aae892df2f92e7ce7c
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-07-01 17:36:22 -0300

    script para povoamento do bd

---
+++ docs/bd/script_povoamento.sql
@@ -0,0 +1,76 @@
+
+use bbcal;
+
+-- insert dos tipos de alimento
+
+INSERT INTO TipoAlimento(Categoria) values("Sólido");
+INSERT INTO TipoAlimento(Categoria) values("Líquido");
+INSERT INTO TipoAlimento(Categoria) values("Pastoso");
+
+-- insert dos alimentos
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo) 
+VALUES ("Feijão Cozido",76.0,5.04,13.61,100,0.85,2.6,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo) 
+VALUES ("Arroz Branco Cozido",128.3,5.04,2.08,100,0.2,1.6,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Mandioca Cozida",125.4,0.6,30.1,100,0.3,1.6,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Abóbora Cabotian cozida",38.6,1.8,8.4,100,0.5,2.2,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Diamante Negro",128.0,1.4,17.0,100,3.7,0.2,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Acém Moído Cozido",212.4,26.7,4.2,100,3.7,4.8,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Pão Francês",299.8,0.5,58.7,100,1.2,0.9,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Suco de soja maça",33.5,0.6,18.6,100,1.8,0.5,NULL,2);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Leite UHT Semidesnatado",78.0,6.0,9.0,100,4.2,0.2,NULL,2);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Patê de Atum",67.0,4.6,3.7,100,4.8,0.7,NULL,3);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Doce de Leite",267.0,3.5,4.0,100,6.8,0.4,NULL,3);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Molho de Tomate",25.0,1.5,2.8,100,2.8,0.9,NULL,3);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Ovo cozido",92.0,6.6,2.2,100,4.8,0.4,NULL,1);
+
+INSERT INTO Alimento(nome,valor_cal,quantidade_proteina,quantidade_carboidrato,porcao,teor_limpidico,teor_fibroso,imagem_ref,TipoAlimento_idTipo)
+VALUES ("Manteiga sem sal",757.5,0.4,1.5,100,86.0,0.1,NULL,3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
